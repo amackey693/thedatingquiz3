fa98bc97acf1416d7145b35c1b36991c4c9c1576
Maria Lagunero <maria58@gmail.com>
2020-03-26 15:23:27 -0700

    add more scripts still not working add img to file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-$(document).ready(function)
+// back end logic 
+// var add = function(colorChoice, favDrink, favFood) {
+//   return colorChoice + favDrink + favFood;
+// };
+
+$(document).ready(function() {
+
+  $("form#dating").submit(function(event){
+    var colorChoice = parseInt($("colorChoice").val());
+    var favDrink = parseInt($("favDrink").val());
+    var favFood = parseInt($("favFood").val());
+    var result = (colorChoice + favDrink + favFood);
+
+    // var result = add(colorChoice + favDrink + favFood);
+
+        
+    // if (colorChoice, favDrink, favFood = 0) {
+    //   $("#has-warning").show()
+    // } else if (result <= 3) {
+
+    if (result <=3) {
+      $('#chris').show(); 
+    } else if (result <= 6) {
+      $('#michelle').show();
+    } else if (result <=9) {
+      $('#patrick').show(); 
+    };
+    console.log();
+
+  event.preventDefault();
+  return (result)
+  });
+});
