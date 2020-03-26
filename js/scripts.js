a8e8501e89175c1ddf5f049faa1362c9e2136305
Maria Lagunero <maria58@gmail.com>
2020-03-26 16:07:20 -0700

    added correct file from pair everything current still not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,27 @@
-// back end logic 
-// var add = function(colorChoice, favDrink, favFood) {
-//   return colorChoice + favDrink + favFood;
-// };
-
 $(document).ready(function() {
 
-  $("form#dating").submit(function(event){
-    var colorChoice = parseInt($("colorChoice").val());
-    var favDrink = parseInt($("favDrink").val());
-    var favFood = parseInt($("favFood").val());
+  $("form#dating").submit(function(event) {
+    var colorChoice = parseInt($("#colorChoice").val());
+    var favDrink = parseInt($("#favDrink").val());
+    var favFood = parseInt($("#favFood").val());
     var result = (colorChoice + favDrink + favFood);
+    
 
-    // var result = add(colorChoice + favDrink + favFood);
-
-        
-    // if (colorChoice, favDrink, favFood = 0) {
-    //   $("#has-warning").show()
-    // } else if (result <= 3) {
-
-    if (result <=3) {
+    // if (colorChoice === 0 || favDrink === 0 || favFood === 0) {
+    //   $(".has-warning").show()
+    // } else 
+    
+    if (result <= 3) {
       $('#chris').show(); 
     } else if (result <= 6) {
       $('#michelle').show();
     } else if (result <=9) {
       $('#patrick').show(); 
-    };
-    console.log();
-
-  event.preventDefault();
-  return (result)
-  });
+    } else {
+      console.log('something is very wrong!')
+    }
+    
+    event.preventDefault();
+    return (result)
+  )}
 });
