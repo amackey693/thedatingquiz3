461675540d2f2a7b092619a9fa57b79ac511af84
Maria Lagunero <maria58@gmail.com>
2020-03-26 17:35:30 -0700

    added attributes selected to if statements

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,29 @@
 
 $(document).ready(function() {
 
+  // $('.form-control').on('change', function(){
+  //   $('#favDrink').val($(this).val());
+  // })
+  // $('.form-control').change()
+
   $("form#dating").submit(function(event){
     console.log('hello')
+    event.preventDefault();
     
     var colorChoice = parseInt($("#colorChoice").val());
     var favDrink = parseInt($("#favDrink").val());
     var favFood = parseInt($("#favFood").val());
     var result = (colorChoice + favDrink + favFood);
+
     console.log(colorChoice)
     console.log(favDrink)
     console.log(favFood)
-    
-    // var result = add(colorChoice + favDrink + favFood);
 
-        
-    // if (colorChoice === 0 || favDrink === 0 || favFood === 0) {
-    //   $("#has-warning").show()
-    // } else if (result <= 3) {
-
-    // if (colorChoice === 0 || favDrink === 0 || favFood === 0) {
-    //   $(".has-warning").show();
-    // } else if (result <=3) {
-    //   $('#chris').show(); 
-    // } else if (result <= 6) {
-    //   $('#michelle').show();
-    // } else if (result <=9) {
-    //   $('#patrick').show(); 
-    // };
-
-    if (result <=3) {
+    if (colorChoice === 0 || favDrink === 0 || favFood === 0) {
+      $(".has-warning").show();
+      $(".has-warning").attr("selected","selected");
+      $('#chris, #michelle, #patrick').hide();
+    } else if (result <=3) {
       $('#chris').show();  
       $('#michelle, #patrick').hide();
     } else if (result <= 6) {
@@ -43,13 +37,8 @@ $(document).ready(function() {
     } else if (result <=9) {
       $('#patrick').show();
       $('#chris, #michelle').hide();
-
-    } else if (colorChoice === 0 || favDrink === 0 || favFood === 0) {
-      $(".has-warning").show();
-      $('#chris, #michelle, #patrick').hide();
     };
     console.log();
-    event.preventDefault();
     return (result)
   })  
 });
